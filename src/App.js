8cd872ddc0ff1676836fb88a401815def20bc048
FatimaMary <111224454+FatimaMary@users.noreply.github.com>
2022-11-18 18:44:29 +0530

    Context

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Header from "./Header"
+import {UserContextConsumer} from "./userContext"
+
+class App extends React.Component {
+    state = {
+        newUsername: ""
+    }
+    
+    handleChange = (e) => {
+        const {name, value} = e.target
+        this.setState({[name]: value})
+    }
+    
+    render() {    
+        return (
+            <div>
+                <Header />
+                    <UserContextConsumer>
+                        {({username, changeUsername}) => (
+                            <main>  
+                                <p className="main">No new notifications, {username}! 🎉</p>
+                                <input
+                                    type="text"
+                                    name="newUsername"
+                                    placeholder="New username"
+                                    value={this.state.newUsername}
+                                    onChange={this.handleChange}
+                                />
+                                <button onClick={() => changeUsername(this.state.newUsername)}>Change Username</button>
+                            </main> 
+                        )}
+                    </UserContextConsumer>
+            </div>
+        )
+    }
 }
 
-export default App;
+export default App
